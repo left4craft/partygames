cddc063849637f7fde0b0f63d73c5179e568485e
Trevor Wong <trevor8568@gmail.com>
2020-12-27 16:44:10 -0500

    Hello world

--- src/main/java/me/sisko/partygames/Main.java
+++ src/main/java/me/sisko/partygames/Main.java
@@ -1,11 +1,23 @@
 package me.sisko.partygames;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+import net.md_5.bungee.api.ChatColor;
+
+public class Main extends JavaPlugin implements Listener {
     
     @Override
     public void onEnable() {
         getLogger().info("Hello World!");
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        e.getPlayer().sendMessage(ChatColor.GREEN + "Hello world!");
     }
 }
