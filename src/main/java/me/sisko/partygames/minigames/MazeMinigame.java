3c5aae6d856ec228d2ddb4ab55ddd6d04f53f859
Trevor Wong <trevor8568@gmail.com>
2021-01-05 13:09:23 -0500

    add maze minigame template

---
+++ src/main/java/me/sisko/partygames/minigames/MazeMinigame.java
@@ -0,0 +1,76 @@
+package me.sisko.partygames.minigames;
+
+import java.util.List;
+
+import org.bukkit.entity.Player;
+import org.json.JSONObject;
+
+public class MazeMinigame extends Minigame {
+
+    @Override
+    public boolean jsonValid(JSONObject json) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void setup(JSONObject json) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void initialize() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void prestart(List<Player> players) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void postgame() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void cleanup() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public List<Player> timeout() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void addPlayer(Player p) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void removePlayer(Player p) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public List<String> getScoreboardLines(Player p) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
